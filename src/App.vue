ca1ab13b971b6f712d028b8d5dba4dc12b96ffa6
kon <74819209+kon2300@users.noreply.github.com>
2021-02-14 09:42:06 +0900

    課題③完成

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppHeader />
+  <AppTaskRadioButton />
+  <AppTaskList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'AppHeader': require('@/components/AppHeader.vue').default,
+    'AppTaskRadioButton': require('@/components/AppTaskRadioButton.vue').default,
+    'AppTaskList': require('@/components/AppTaskList.vue').default
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    padding: 5px;
+  }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app-header">
+    <h1 class="is-size-1">ToDo リスト</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
